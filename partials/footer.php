777afa2998e832d744631109ab8761f0e1dcf645
Spoonful-Creative <pughsinead@hotmail.com>
2017-03-22 20:47:19 +1300

    Fixing datepicker (NOT compatible with CHROME)

--- partials/footer.php
+++ partials/footer.php
@@ -4,32 +4,39 @@
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
 
-<!-- <script src="js/main.js"></script> -->
+<script src="js/main.js"></script>
 
 <script type="text/javascript">
-var now = new Date();
-var today = new Date(now).toLocaleDateString('en-NZ', {
-    day : 'numeric',
-    month : 'numeric',
-    year : 'numeric'
-}).split(' ').join('/');
-now.setFullYear( now.getFullYear() - 150 );
+// var now = new Date();
+// var today = new Date(now).toLocaleDateString('en-NZ', {
+//     day : 'numeric',
+//     month : 'numeric',
+//     year : 'numeric'
+// }).split(' ').join('/');
+// now.setFullYear( now.getFullYear() - 150 );
 
-var olddate = new Date(now).toLocaleDateString('en-NZ', {
-    day : 'numeric',
-    month : 'numeric',
-    year : 'numeric'
-}).split(' ').join('/');
-console.log($('.datepicker').pickadate({
-	format: 'dd/mm/yyyy',
+// var olddate = new Date(now).toLocaleDateString('en-NZ', {
+//     day : 'numeric',
+//     month : 'numeric',
+//     year : 'numeric'
+// }).split(' ').join('/');
+// $('.datepicker').pickadate({
+// 	format: 'dd/mm/yyyy',
+//     selectMonths: true, 
+//     selectYears: 150,
+//     min: olddate,
+//     max: today
+//   });
+
+var today = new Date();
+today.setFullYear( today.getFullYear() - 150 );
+$('.datepicker').pickadate({
+	format: 'mm/dd/yyyy',
     selectMonths: true, 
     selectYears: 150,
-    min: olddate,
-    max: today,
-    value: document.getElementById('dateofbirth').value
-  }));
-
-
+    min: today,
+    max: new Date()
+  });
 
 $(document).ready(function() {
     $('select').material_select();
--- partials/thanks.php
+++ partials/thanks.php
@@ -1,29 +1,28 @@
 <?php include 'header.php'; ?>
 <div id="center-page">
-<div class="container" id="thanks-page">
-	
-	<div class="row">
-		<div class="col s10 offset-s1 m8 offset-m2 l8 offset-l2">
-			<div class="center">
-				<img src="img/thanks-icon.png" />
-			</div>
+	<div class="container" id="thanks-page">
 		
-				
-				<h2 class="text-center">Thanks for the message </br><span class="highlight"><?= $name ?></span></h2>
-				<h2 class="text-center text-muted">We will be in contact shortly via </br><span class="highlight"><?= $email ?></span></h2>
+		<div class="row">
+			<div class="col s10 offset-s1 m8 offset-m2 l8 offset-l2">
+				<div class="center">
+					<img id="open-mail" src="img/thanks-icon.png" />
+				</div>
+			
+					
+					<h2 class="text-center">Thanks for the message </br><span class="highlight"><?= $name ?></span></h2>
+					<h2 class="text-center text-muted">We will be in contact shortly via </br><span class="highlight"><?= $email ?></span></h2>
 
 
-				<div class="right">
-				<a href="./index.php">
-					<button id="resetForm" class="btn waves-effect waves-light" type="submit" name="action">
-					  Return
-					  <i class="material-icons right">replay</i>
-				 </button>
-				 </a>
+					<div class="right">
+					<a href="./index.php" id="resetForm" class="btn waves-effect waves-light" type="submit" name="action">
+						  Return
+						  <i class="material-icons right">replay</i>
+					</a>
+					</div>
 				</div>
 			</div>
 		</div>
-</div>
+	</div>
 </div>
 
 <?php include 'footer.php'; ?>
