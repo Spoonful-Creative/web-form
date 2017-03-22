2c41483a097ba0806d299887699169f847706859
Spoonful-Creative <pughsinead@hotmail.com>
2017-03-22 21:24:09 +1300

    Adding comments/cleaning code = HEADER WORKING

--- partials/header.php
+++ partials/header.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
+
 <html lang="en-nz">
+
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-	<title>PHP Form Assessment</title>
+	<title>Get In Touch!</title>
 
 	<!-- Compiled and minified CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
 
-	<!--Import Google Icon Font-->
+	<!--Google Fonts-->
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	
+	<!--Web Form CSS-->
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
+
 
 <body>
