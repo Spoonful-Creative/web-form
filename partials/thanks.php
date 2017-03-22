5e2972b8ca1ade3d4de0201cca36c42cd4fb8a3b
Spoonful-Creative <pughsinead@hotmail.com>
2017-03-22 21:33:50 +1300

    Adding comment/cleaning code = THANKS WORKING

--- partials/thanks.php
+++ partials/thanks.php
@@ -1,12 +1,13 @@
 <?php include 'header.php'; ?>
-<div id="center-page">
-	<div class="container" id="thanks-page">
-		
-		<div class="row">
-			<div class="col s10 offset-s1 m8 offset-m2 l8 offset-l2">
-				<div class="center">
-					<img id="open-mail" src="img/thanks-icon.png" />
-				</div>
+	
+	<div id="center-page">
+		<div class="container" id="thanks-page">
+			
+			<div class="row">
+				<div class="col s10 offset-s1 m8 offset-m2 l8 offset-l2">
+					<div class="center">
+						<img id="open-mail" src="img/thanks-icon.png" />
+					</div>
 			
 					
 					<h2 class="text-center">Thanks for the message </br><span class="highlight"><?= $name ?></span></h2>
@@ -14,16 +15,13 @@
 
 
 					<div class="right">
-					<a href="./index.php" id="resetForm" class="btn waves-effect waves-light" type="submit" name="action">
-						  Return
-						  <i class="material-icons right">replay</i>
-					</a>
+						<a href="./index.php" id="resetForm" class="btn waves-effect waves-light" type="submit" name="action">
+							  Return <i class="material-icons right">replay</i>
+						</a>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-
 <?php include 'footer.php'; ?>
 
